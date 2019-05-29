31c457e15e99b6f0600de96e94258e7f43dcc9d1
alialkhateeb <alkhateeb.ali.m@gmail.com>
2019-05-29 10:49:24 -0400

    Mastermind project

---
+++ Mastermind/Master.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+
+namespace Mastermind
+{
+    class Master
+    {
+        public static int[] secret;
+        
+
+        public static void generatesecret()
+        {
+            secret = new int[4];
+            
+            Random rand = new Random();
+            for (int i = 0; i < secret.Length; i++)
+            {
+                secret[i] = rand.Next(6) + 1;
+            }
+        }
+        public static int validateData(char[] checker)
+        {
+            int counter = 0;
+            for (int i = 0; i < checker.Length; i++)
+            {
+                if (checker[i] == '+')
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        public static char[] parseInput(string value)
+        {
+            List<char> listChar = new List<char>();
+            char[] checker = new char[4];
+            listChar.AddRange(value.ToCharArray());
+            
+            if (value.Length != 4)
+            {
+                throw new LongerThanFourException("Please Enter 4 digits from 1 to 6 inclusive: ");
+            }
+
+
+            else if (listChar.Exists(z => z < '1' || z > '6'))
+            {
+                throw new InvalidInputException("the input string contain invalid character.");
+            }
+            else
+            {
+                for (int i = 0; i < listChar.Count; i++)
+                {
+                    if (Array.Exists(secret, s => s.Equals(Convert.ToInt32(listChar[i].ToString()))))
+                    {
+                        if (Convert.ToInt32(listChar[i].ToString()) == secret[i])
+                        {
+                            checker[i] = '+';
+                        }
+                        else
+                        {
+                            checker[i] = '-';
+                        }
+
+                    }else
+                    {
+                        checker[i] = 'x';
+                    }
+                    
+                }
+            }
+            
+
+            return checker;
+        }
+
+    }
+    public class LongerThanFourException: Exception
+    {
+        public LongerThanFourException(string message): base(message)
+        {
+
+        }
+    }
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Mastermind/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Master.generatesecret();
+            List<int> lis = new List<int>();
+            Console.WriteLine("Please Enter 4 digits from 1 to 6 inclusive: ");
+            
+            try
+            {
+                
+                for (int i = 0; i < 10; i++)
+                {
+                    var entered = Console.ReadLine();
+                    
+                    char[] checker = Master.parseInput(entered);
+                    Console.WriteLine(checker);
+                    
+                    if (Master.validateData(checker) == 4)
+                    {
+                        Console.WriteLine("You Won!!");
+                        break;
+                    }
+                    else
+                    {
+                        if (i < 9)
+                        {
+                        Console.WriteLine("Not quite right, keep trying!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You have lost the game.");
+                        }
+                    }
+                }
+
+            }
+            catch (LongerThanFourException e)
+            {
+                Console.WriteLine("Invalid value, " + e.Message);
+                
+            }
+            catch(InvalidInputException e)
+            {
+                Console.WriteLine("Invalid value, " + e.Message);
+            }
+            
+            
+        }
+    }
+}
